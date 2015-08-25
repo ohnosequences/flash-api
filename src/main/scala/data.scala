d23d8a26d28f12d8ad268a169aaa476b9f8e624f
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2015-08-25 19:17:15 +0200

    add merged reads and constructors for all data

--- src/main/scala/data.scala
+++ src/main/scala/data.scala
@@ -5,6 +5,7 @@ import api._
 
 case object data {
 
+  // data types
   case object MergedReadStatsType extends AnyDataType {
 
     type Record = mergedStats.type
@@ -13,9 +14,31 @@ case object data {
     val label = toString
   }
 
-  trait AnyMergedReadStats extends AnyData {
+  // In a future world this could link to the illumina read type
+  trait AnyMergedReadsType extends AnyDataType
+
+  // data
+  trait AnyMergedReads extends AnyData {
+
+    type DataType <: AnyMergedReadsType
+
+    type FlashExpression <: AnyFlashExpression
+    val flashExpression: FlashExpression
+  }
+  abstract class MergedReads[DT <: AnyMergedReadsType, FE <: AnyFlashExpression](
+    val dataType: DT,
+    val flashExpression: FE,
+    val label: String
+  ) extends AnyMergedReads {
+
+    type DataType = DT
+    type FlashExpression = FE
+  }
+
+  abstract class MergedReadStats(val label: String) extends AnyData {
 
     type DataType = MergedReadStatsType.type
     val dataType = MergedReadStatsType
   }
+
 }
